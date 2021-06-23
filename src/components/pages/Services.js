cd6acafbad7c1fcf4680654310087cd9ba0f3dba
georgeokez <georgeokez@gmail.com>
2021-06-23 18:37:25 +0100

    added the services page

---
+++ src/components/pages/Services.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from '../heroSection/HeroSection';
+import {homeObjThree} from './Data';
+
+function Services() {
+    return (
+        <>
+           <HeroSection {...homeObjThree} /> 
+        </>
+    )
+}
+
+export default Services
